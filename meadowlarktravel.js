8c15381be329b9c147a81995af39c574016b127d
at3158 <at3158@hotmail.com>
2016-03-09 16:24:10 +0800

    add fortune.js

--- meadowlarktravel.js
+++ meadowlarktravel.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -12,26 +13,15 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortuneCookies = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/index', function(req,res){
-	var randomFortune = 
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('index', { fortune: randomFortune });
+	res.render('index',  { fortune: fortune.getFortune()});
 });
 app.get('/about', function(req,res){
-	var randomFortune = 
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune()} );
 });
 
 // 404 catch-all handler (middleware)
