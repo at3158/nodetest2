f4e299ded5efce3391f087d12ff760b5148d60dc
at3158 <at3158@hotmail.com>
2016-03-09 17:28:15 +0800

    add test Mocha chai Zombie Grunt

--- meadowlarktravel.js
+++ meadowlarktravel.js
@@ -3,9 +3,17 @@ var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-// set up handlebars view engine
-var handlebars = require('express3-handlebars')
-	.create({ defaultLayout:'main' });
+//set up handlebars view engine
+var handlebars = require('express3-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -13,6 +21,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+//set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
+});
 
 app.get('/', function(req, res) {
 	res.render('home');
@@ -21,7 +35,20 @@ app.get('/index', function(req,res){
 	res.render('index',  { fortune: fortune.getFortune()});
 });
 app.get('/about', function(req,res){
-	res.render('about', { fortune: fortune.getFortune()} );
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 // 404 catch-all handler (middleware)
