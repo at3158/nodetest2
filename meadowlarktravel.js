3d3af4d56a87792768d512ca594844a762bed37b
at3158 <at3158@hotmail.com>
2016-03-15 11:53:43 +0800

    add ch07 template-weather

--- meadowlarktravel.js
+++ meadowlarktravel.js
@@ -28,7 +28,47 @@ app.use(function(req, res, next){
 	next();
 });
 
+//mocked weather data
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+ 	res.locals.partials.weatherContext = getWeatherData();
+ 	next();
+});
+
+
 app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/home', function(req, res) {
 	res.render('home');
 });
 app.get('/index', function(req,res){
@@ -49,6 +89,21 @@ app.get('/tours/oregon-coast', function(req, res){
 });
 app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
+});
+
+app.get('/jquery-test', function(req, res){
+	res.render('jquery-test');
+});
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
 });
 
 // 404 catch-all handler (middleware)
